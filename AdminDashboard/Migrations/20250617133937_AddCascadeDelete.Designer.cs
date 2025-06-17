a5adb467f905a5c1a34e893891338ed03a24bcf1
Pavel Karpov <pavelkarpov02@yandex.by>
2025-06-17 16:44:06 +0300

    update database

--- AdminDashboard/Migrations/20250617133937_AddCascadeDelete.Designer.cs
+++ AdminDashboard/Migrations/20250617133937_AddCascadeDelete.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AdminDashboard.DbStuff;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AdminDashboard.Migrations
 {
     [DbContext(typeof(SocialNetworkWebDbContext))]
-    partial class SocialNetworkWebDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250617133937_AddCascadeDelete")]
+    partial class AddCascadeDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.17");
---
+++ AdminDashboard/Migrations/20250617133937_AddCascadeDelete.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdminDashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTokens_Tokens_TokenId",
+                table: "UserTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTokens_Users_UserId",
+                table: "UserTokens");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTokens_Tokens_TokenId",
+                table: "UserTokens",
+                column: "TokenId",
+                principalTable: "Tokens",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTokens_Users_UserId",
+                table: "UserTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTokens_Tokens_TokenId",
+                table: "UserTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTokens_Users_UserId",
+                table: "UserTokens");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Users_UserId",
+                table: "Payments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTokens_Tokens_TokenId",
+                table: "UserTokens",
+                column: "TokenId",
+                principalTable: "Tokens",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTokens_Users_UserId",
+                table: "UserTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
